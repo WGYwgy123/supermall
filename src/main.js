031649ac3945bcb7aee631bbafb16b7d7d5c513e
wgy <709581924@qq,com>
2020-05-13 17:04:28 +0800

    '完成首页和详情页'

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+//这里设置总线
+Vue.prototype.$bus = new Vue()
+
 new Vue({
   render: h => h(App),
   router,
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,122 +1,25 @@
 <template>
-  <div id="home">
+  <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 
+    <tab-control :titles="['流行','新款','精选']"
+                 @tabClick="tabClick"
+                 ref="tabControl1"
+                 class="tab-control" v-show="isTabFixed"/>
     <scroll class="content"
             ref="scroll"
             :probe-type="3"
             @scroll="contentScroll"
             :pull-up-load="true"
             @pullingUp="loadMore">
-      <home-swiper :banners="banners"/>
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
       <recommend-view :recommends="recommends"/>
       <feature-view/>
-      <tab-control class="tab-control"
-                   :titles="['流行','新款','精选']"
-                   @tabClick="tabClick"/>
+      <tab-control :titles="['流行','新款','精选']"
+                   @tabClick="tabClick"
+                   ref="tabControl2" />
+      <!--:class="{fixed : isTabFixed} 当isTabFixed为true的时候class里面会加上fixed-->
       <goods-list :goods="showGoods"/>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>11</li>
-        <li>12</li>
-        <li>13</li>
-        <li>14</li>
-        <li>15</li>
-        <li>16</li>
-        <li>17</li>
-        <li>18</li>
-        <li>19</li>
-        <li>20</li>
-        <li>21</li>
-        <li>22</li>
-        <li>23</li>
-        <li>24</li>
-        <li>25</li>
-        <li>26</li>
-        <li>27</li>
-        <li>28</li>
-        <li>29</li>
-        <li>30</li>
-        <li>31</li>
-        <li>32</li>
-        <li>33</li>
-        <li>34</li>
-        <li>35</li>
-        <li>36</li>
-        <li>37</li>
-        <li>38</li>
-        <li>39</li>
-        <li>40</li>
-        <li>41</li>
-        <li>42</li>
-        <li>43</li>
-        <li>44</li>
-        <li>45</li>
-        <li>46</li>
-        <li>47</li>
-        <li>48</li>
-        <li>49</li>
-        <li>50</li>
-        <li>51</li>
-        <li>52</li>
-        <li>53</li>
-        <li>54</li>
-        <li>55</li>
-        <li>56</li>
-        <li>57</li>
-        <li>58</li>
-        <li>59</li>
-        <li>60</li>
-        <li>61</li>
-        <li>62</li>
-        <li>63</li>
-        <li>64</li>
-        <li>65</li>
-        <li>66</li>
-        <li>67</li>
-        <li>68</li>
-        <li>69</li>
-        <li>70</li>
-        <li>71</li>
-        <li>72</li>
-        <li>73</li>
-        <li>74</li>
-        <li>75</li>
-        <li>76</li>
-        <li>77</li>
-        <li>78</li>
-        <li>79</li>
-        <li>80</li>
-        <li>81</li>
-        <li>82</li>
-        <li>83</li>
-        <li>84</li>
-        <li>85</li>
-        <li>86</li>
-        <li>87</li>
-        <li>88</li>
-        <li>89</li>
-        <li>90</li>
-        <li>91</li>
-        <li>92</li>
-        <li>93</li>
-        <li>94</li>
-        <li>95</li>
-        <li>96</li>
-        <li>97</li>
-        <li>98</li>
-        <li>99</li>
-        <li>100</li>
-      </ul>
     </scroll>
 
     <back-top @click.native="backClick" v-show="isShowBackTop"/>
@@ -135,6 +38,7 @@
   import BackTop from "components/content/backTop/BackTop";
 
   import {getHomeMultidata, getHomeGoods} from 'network/home';
+  import {debounce} from "common/utils";
 
   export default {
     name: "Home",
@@ -158,13 +62,28 @@
           'sell': {page: 0, list: []}
         },
         currentType: 'pop',
-        isShowBackTop: false
+        isShowBackTop: false,
+        tabOffsetTop: 0,
+        isTabFixed: false,
+        saveY: 0
       }
     },
     computed: {
       showGoods() {
         return this.goods[this.currentType].list
       }
+    },
+    destroyed() {
+      //console.log('home destroyed');
+    },
+    activated() {
+      this.$refs.scroll.refresh();
+      this.$refs.scroll.scrollTo(0, this.saveY, 0)
+      //console.log('activated');
+    },
+    deactivated() {
+      this.saveY = this.$refs.scroll.getScrollY()
+      //console.log('deactivated');
     },
     created() {
       //1.请求多个数据
@@ -174,12 +93,27 @@
       this.getHomeGoods('pop')
       this.getHomeGoods('new')
       this.getHomeGoods('sell')
+
+    },
+    mounted() {
+      //1. 图片加载完成的事件监听
+      const refresh = debounce(this.$refs.scroll.refresh, 500)
+
+      //监听item中图片加载完成
+      this.$bus.$on('itemImageLoad', () => {
+        refresh()
+      })
+
+
     },
     methods: {
 
       /**
        * 事件监听相关的方法
        */
+
+
+
       tabClick(index) {
         switch (index) {
           case 0:
@@ -192,15 +126,28 @@
             this.currentType = 'sell'
             break
         }
+        this.$refs.tabControl1.currentIndex = index;
+        this.$refs.tabControl2.currentIndex = index;
       },
       backClick() {
         this.$refs.scroll.scrollTo(0,0,500);
       },
       contentScroll(position) {
-        this.isShowBackTop = -position.y > 1000
+        //1.判断backtop是否显示
+        this.isShowBackTop = (-position.y) > 1000
+
+        //2.决定tabcontrol是否吸顶
+        this.isTabFixed = (-position.y) > this.tabOffsetTop
       },
       loadMore() {
+        console.log('下拉加载更多');
         this.getHomeGoods(this.currentType)
+      },
+      swiperImageLoad() {
+        //console.log(this.$refs.tabControl2.$el.offsetTop)
+        //2. 获取tabControl的offsetTop
+        // 所有的组件都有一个属性$el 用于获取属性的元素
+        this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop;
       },
 
       /**
@@ -217,8 +164,8 @@
         const page = this.goods[type].page + 1
         getHomeGoods(type,page).then(res => {
           //下面这个push是es6语法，将res.data.list里的数组挨个塞到goods中 没接口所以暂时代替它
-          //this.goods[type].list.push(...res.data.list)
-          this.goods[type].list.push(1,2,3,4,5,6)
+          this.goods[type].list.push(...res.data.list)
+          //this.goods[type].list.push(1,2,3,4,5,6)
           this.goods[type].page += 1
 
           this.$refs.scroll.finishPullup()
@@ -233,16 +180,18 @@
     /*padding-top: 44px;*/
 
     height: 100vh;
+    position: relative;
   }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
 
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
-    z-index: 9;
+    /*使用浏览器原生滚动时为了让导航不滚动
+    position: fixed;*/
+    /*left: 0;*/
+    /*right: 0;*/
+    /*top: 0;*/
+    /*z-index: 9;*/
   }
   .tab-control {
     position: sticky;
